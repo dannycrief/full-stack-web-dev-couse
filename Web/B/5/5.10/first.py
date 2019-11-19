47a9c249380858bee2f85c81aef9c7836ecdec1a
dannycrief <step.kozbvb@icloud.com>
2019-11-19 14:21:06 +0100

    File was updated.

--- Web/B/5/5.10/first.py
+++ Web/B/5/5.10/first.py
@@ -10,8 +10,21 @@ class Node(object):
 def print_linked_list(head):
     cur = head
     while cur is not None:
-        print(cur)
         cur = cur.next
+    return head
+
+
+def reversed_linked_list(head):
+    cur = head
+    prev = None
+    while cur is not None:
+        nxt = cur.next
+        cur.next = prev
+
+        prev = cur
+        cur = nxt
+    head = prev
+    return head
 
 
 h, a, b, c, d = Node(1), Node(2), Node(3), Node("Внезапно"), Node(5)
@@ -19,4 +32,7 @@ h.next = a
 a.next = b
 b.next = c
 c.next = d
+print(print_linked_list(h))
+h = reversed_linked_list(h)
+print("---")
 print(print_linked_list(h))
