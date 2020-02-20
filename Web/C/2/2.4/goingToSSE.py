fa197f0fd0b0d2a56abbae77a653cf3a3370c0a5
danny <step.kozbvb@gmail.com>
2020-02-20 12:37:44 +0200

    20.02.2020 12:37

---
+++ Web/C/2/2.4/goingToSSE.py
@@ -0,0 +1,19 @@
+import time
+import bottle
+
+
+@bottle.route('/words')
+def word_spammer():
+    bottle.response.content_type = "text/event-stream"
+    bottle.response.cache_control = "no-cache"
+    bottle.response.headers['Access-Control-Allow-Origin'] = '*'
+
+    words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven']
+    for word in words:
+        yield 'data: %s\n\n' % word
+        time.sleep(2)
+
+
+# http --stream localhost:8080/words
+if __name__ == "__main__":
+    bottle.run(server='gunicorn')
