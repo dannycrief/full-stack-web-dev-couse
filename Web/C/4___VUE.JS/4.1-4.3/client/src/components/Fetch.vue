50b12c885e6cb5ee5d3015bd3a99b4d198192583
danny <step.kozbvb@gmail.com>
2020-02-29 01:12:51 +0100

    29.02.20 01:09AM Vue.js begging

---
+++ Web/C/4___VUE.JS/4.1-4.3/client/src/components/Fetch.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Fetch',
+  data() {
+    return {
+      msg: 'Всё работает',
+    };
+  },
+};
+</script>
