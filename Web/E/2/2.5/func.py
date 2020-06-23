18eeb6ef702e5f5039de45c1d052df2ba3509d20
Stepan Kozurak <step.kozbvb@gmail.com>
2020-06-23 16:38:40 +0200

    e2.6

--- Web/E/2/2.5/func.py
+++ Web/E/2/2.5/func.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 # lambda arguments: function
 a = lambda x: x
 
@@ -13,3 +15,29 @@ print(list(map(lambda x: x * x, l)))  # [1, 4, 9]
 ll = [-1, -2, 3, 4, -22]
 print(list(filter(lambda x: x > 0, ll)))  # [3, 4]
 
+
+def linear(a, b, x):
+    # print(a, x, b)
+    return a * x + b
+
+
+linear1 = partial(linear, 1, 1)
+linear2 = partial(linear, -1, -1)
+print(linear1(2))  # 3
+print(linear2(2))  # -3
+
+
+def in_between(a, b, c):
+    return a < b < c
+
+
+ll = [1, 2, 3, 4, 5, 6]
+z = list(filter(partial(in_between, 3, c=5), ll))
+print(z)
+
+
+def multiply(x, y):
+    return x * y
+
+
+print(partial(multiply, 2))
